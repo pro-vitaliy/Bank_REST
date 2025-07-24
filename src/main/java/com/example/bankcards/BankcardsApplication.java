c78cbc3b256d1e42cd8f1dfe10da3b1e7a0af383
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-24 22:15:42 +0500

    enable Jpa auditing in main class

--- src/main/java/com/example/bankcards/BankcardsApplication.java
+++ src/main/java/com/example/bankcards/BankcardsApplication.java
@@ -2,7 +2,9 @@ package com.example.bankcards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BankcardsApplication {
     public static void main(String[] args) {
