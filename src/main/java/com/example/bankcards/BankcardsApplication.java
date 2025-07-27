0037fe5c6e52bbfc03c317af4cd892e113283e31
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-27 19:59:38 +0500

    update main class enable Retry and Scheduling

--- src/main/java/com/example/bankcards/BankcardsApplication.java
+++ src/main/java/com/example/bankcards/BankcardsApplication.java
@@ -3,7 +3,11 @@ package com.example.bankcards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableRetry
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class BankcardsApplication {
