c3afa5ff44ced24112efa73e9dfb382bdbfe6220
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-28 04:13:12 +0500

    add roles to generated token

--- src/main/java/com/example/bankcards/util/JwtUtils.java
+++ src/main/java/com/example/bankcards/util/JwtUtils.java
@@ -8,20 +8,23 @@ import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class JwtUtils {
     private final JwtEncoder encoder;
 
-    public String generateToken(String username) {
+    public String generateToken(String username, List<String> roles) {
         Instant now = Instant.now();
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(now)
                 .expiresAt(now.plus(1, ChronoUnit.DAYS))
                 .subject(username)
+                .claim("roles", roles)
                 .build();
+
         return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 }
