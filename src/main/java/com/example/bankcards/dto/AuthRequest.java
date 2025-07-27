fe0febf57fa5a6dffdc5964505279ce756246926
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-27 20:19:28 +0500

    add AuthRequest DTO

---
+++ src/main/java/com/example/bankcards/dto/AuthRequest.java
@@ -0,0 +1,16 @@
+package com.example.bankcards.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequest {
+
+    @NotBlank(message = "Имя пользователя не должно быть пустым")
+    private String username;
+
+    @NotBlank(message = "Пароль не должен быть пустым")
+    private String password;
+}
