1cf853d064c3b3b9889dbd15c5c9e34e02b7b5d8
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-27 20:19:09 +0500

    add JwtUtils for token generation

---
+++ src/main/java/com/example/bankcards/util/JwtUtils.java
@@ -0,0 +1,27 @@
+package com.example.bankcards.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@RequiredArgsConstructor
+@Component
+public class JwtUtils {
+    private final JwtEncoder encoder;
+
+    public String generateToken(String username) {
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.DAYS))
+                .subject(username)
+                .build();
+        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
