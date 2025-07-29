bdfb3fc8916960445b0e469c03d6a026fc2b6eaa
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-07-29 11:43:37 +0500

    update AdminInitializer

--- src/main/java/com/example/bankcards/startup/AdminInitializer.java
+++ src/main/java/com/example/bankcards/startup/AdminInitializer.java
@@ -5,6 +5,7 @@ import com.example.bankcards.entity.Role;
 import com.example.bankcards.repository.UserRepository;
 import com.example.bankcards.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,14 +17,20 @@ public class AdminInitializer implements CommandLineRunner {
     private final UserRepository userRepository;
     private final UserService userService;
 
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
     @Override
     public void run(String... args) throws Exception {
-        if (userRepository.existsByUsername("admin")) {
+        if (userRepository.existsByUsername(adminUsername)) {
             return;
         }
         UserCreateDTO userCreateDTO = new UserCreateDTO();
-        userCreateDTO.setUsername("admin");
-        userCreateDTO.setPassword("password");
+        userCreateDTO.setUsername(adminUsername);
+        userCreateDTO.setPassword(adminPassword);
         userCreateDTO.setRoles(Set.of(Role.ROLE_ADMIN));
 
         userService.create(userCreateDTO);
